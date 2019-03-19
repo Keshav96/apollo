28edf2bc6c9ffa8377dd17d8846fd97b527659ff
Keshav Pawar <46583132+Keshav96@users.noreply.github.com>
2019-03-18 18:38:46 -0700

    Update planning_gflags.cc

--- modules/planning/common/planning_gflags.cc
+++ modules/planning/common/planning_gflags.cc
@@ -65,7 +65,7 @@ DEFINE_string(scenario_narrow_street_u_turn_config_file,
               "scenario/narrow_street_u_turn_config.pb.txt",
               "scenario_narrow_street_u_turn config file");
 
-DEFINE_bool(enable_scenario_side_pass, true,
+DEFINE_bool(enable_scenario_side_pass, false,
             "enable side pass scenario in planning");
 DEFINE_double(side_pass_min_signal_intersection_distance, 50.0,
               "meter, for intersection has signal, ADC will enter side pass "
